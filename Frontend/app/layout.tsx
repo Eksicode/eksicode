6aea9a51b0b35002e557beb1d8d9b1b50aef45aa
mkltkn <mkltkn@gmail.com>
2024-02-16 00:59:56 +0100

    - Version transfer

---
+++ Frontend/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./styles/globals.css";
+import React from "react";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Ekşicode - Yazılımcı Geliştirme Platformu",
+  description:
+    "Telegram grupları üzerinden her seviyeden yazılım geliştiricinin ve yazılım öğrenmek isteyen kişilerin bir araya geldiği platform.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="tr">
+      <body
+        className={
+          process.env.NODE_ENV === "development"
+            ? inter.className + " debug-screens"
+            : "" + " " + inter.className
+        }
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
