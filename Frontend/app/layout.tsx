f10eb32e6cc19b70feed81695fb67290e0d8b210
mkltkn <mkltkn@gmail.com>
2024-02-16 23:56:36 +0100

    Layout conversions

--- Frontend/app/layout.tsx
+++ Frontend/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@styles/globals.css";
 import React from "react";
+import Nav from "../components/Nav";
+import Footer from "../components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,7 +27,9 @@ export default function RootLayout({
             : "" + " " + inter.className
         }
       >
+        <Nav />
         {children}
+        <Footer />
       </body>
     </html>
   );
--- Frontend/tailwind.config.ts
+++ Frontend/tailwind.config.ts
@@ -3,7 +3,7 @@ const colors = require('tailwindcss/colors')
 
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
+    './app/pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
