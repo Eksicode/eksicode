51cff1b9da6049d683d7d1bd17fa7cda5435c32b
mkltkn <mkltkn@gmail.com>
2024-03-30 17:24:10 +0100

    -

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-if [ ! -f "vendor/autoload.php" ]; then
+# if [ ! -f "vendor/autoload.php" ]; then
     composer install --no-progress --no-interaction
-fi
+# fi
 
-if [ ! -f ".env" ]; then
-    echo "Creating env file for env $APP_ENV"
-    cp .env.example .env
-else
-    echo "env file exists."
-fi
+# if [ ! -f ".env" ]; then
+#     echo "Creating env file for env $APP_ENV"
+#     cp .env.example .env
+# else
+#     echo "env file exists."
+# fi
 
 php artisan migrate
 php artisan optimize
