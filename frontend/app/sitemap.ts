658abefd66e1b0cdd21baa079e7f282504d13187
Mkltkn <mkltkn@gmail.com>
2025-01-12 03:05:21 +0100

    -

--- frontend/app/sitemap.ts
+++ frontend/app/sitemap.ts
@@ -1,11 +1,11 @@
-import getPosts from "@providers/getPosts";
+import getData from "@providers/getData";
 
 export default async function sitemap() {
   const baseUrl = "https://eksicode.org";
 
   // get all posts
-  const fetchedPosts = await getPosts("total");
-  const postsUrls = fetchedPosts.map((post: any) => ({
+  const fetchedPosts = await getData("total", false);
+  const postsUrls = fetchedPosts.data.map((post: any) => ({
     url: `${baseUrl}/posts/${post.slug}`,
     lastModified: new Date(),
   }));
