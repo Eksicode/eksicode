a587e68cd18fcb7ed990297226ba14de942012eb
Mkltkn <mkltkn@gmail.com>
2025-01-14 00:22:44 +0100

    -

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  staticPageGenerationTimeout: 180,
   images: {
+    unoptimized: true,
     domains: ['eksicode-images.s3.eu-central-1.amazonaws.com'],
+  },
+  experimental: {
+    workerThreads: true,
+    cpus: 4  // Adjust based on your system
   }
 };
 
