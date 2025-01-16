e6e96f6e0a592fa5ad7bb24c25bf4f652b5de15a
Mkltkn <mkltkn@gmail.com>
2025-01-16 11:29:53 +0100

    -

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   staticPageGenerationTimeout: 180,
+  generateEtags: false,
+  webSocketTimeout: 30000,
   images: {
     unoptimized: true,
     domains: ['eksicode-images.s3.eu-central-1.amazonaws.com'],
   },
   experimental: {
+    serverActions: true,
     workerThreads: true,
     cpus: 4  // Adjust based on your system
   },
