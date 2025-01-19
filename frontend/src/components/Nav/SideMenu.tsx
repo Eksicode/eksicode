79e45710674cc0eb609dc808f601cd0e52cd6a3f
Mkltkn <mkltkn@gmail.com>
2025-01-19 04:17:12 +0100

    -

--- frontend/src/components/Nav/SideMenu.tsx
+++ frontend/src/components/Nav/SideMenu.tsx
@@ -53,16 +53,6 @@ function SideMenu() {
     fetchMenu();
   }, []);
 
-  if (isLoading) {
-    return (
-      <aside className="w-64 sm:hidden" aria-label="Sidebar">
-        <div className="overflow-y-auto flex justify-center w-64 py-4 px-3 bg-gray-50 rounded-lg border-gray-300 border ">
-          <LoadingAnimation />
-        </div>
-      </aside>
-    );
-  }
-
   if (error) {
     return (
       <aside className="w-64 sm:hidden" aria-label="Sidebar">
@@ -75,34 +65,40 @@ function SideMenu() {
 
   return (
     <aside className="w-64 sm:hidden" aria-label="Sidebar">
-      <div className="overflow-y-auto w-64 py-4 px-3 bg-gray-50 rounded-lg border-gray-300 border">
-        <ul className="space-y-2">
-          {menu.length === 0 && (
-            <span className="text-sm font-semibold text-gray-500">
-              Menü bulunamadı
-            </span>
-          )}
-          {menu.map((item) => {
-            const IconComponent = getIconComponent(item.icon);
-            return (
-              <li key={item.id}>
-                <Link
-                  href={item.link}
-                  className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg hover:bg-gray-100"
-                >
-                  {IconComponent && (
-                    <IconComponent
-                      className="text-lg"
-                      {...(IconComponent as any)}
-                    />
-                  )}
-                  <span className="ml-3">{item.name}</span>
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
+      {isLoading ? (
+        <div className="overflow-y-auto min-h-96 flex justify-center w-64 py-4 px-3 bg-gray-50 rounded-lg border-gray-300 border ">
+          <LoadingAnimation />
+        </div>
+      ) : (
+        <div className="overflow-y-auto min-h-96 w-64 py-4 px-3 bg-gray-50 rounded-lg border-gray-300 border">
+          <ul className="space-y-2">
+            {menu.length === 0 && (
+              <span className="text-sm font-semibold text-gray-500">
+                Menü bulunamadı
+              </span>
+            )}
+            {menu.map((item) => {
+              const IconComponent = getIconComponent(item.icon);
+              return (
+                <li key={item.id}>
+                  <Link
+                    href={item.link}
+                    className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg hover:bg-gray-100"
+                  >
+                    {IconComponent && (
+                      <IconComponent
+                        className="text-lg"
+                        {...(IconComponent as any)}
+                      />
+                    )}
+                    <span className="ml-3">{item.name}</span>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      )}
     </aside>
   );
 }
